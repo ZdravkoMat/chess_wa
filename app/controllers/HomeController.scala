6084d5be9a233b898f9e609472c470a2f93a8ca1
Stefan Kugele <stefan.kugele@gmail.com>
2023-10-30 16:53:56 +0100

    Man kann Züge machen

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -3,19 +3,42 @@ package controllers
 import javax.inject._
 import play.api._
 import play.api.mvc._
+import play.twirl.api.Html
+import com.google.inject.Guice
+
 import htwg.se.chess.model.boardComponent.Board
 import htwg.se.chess.controller.Controller
+import htwg.se.chess.util.Observer
+import htwg.se.chess.util.Event
+import htwg.se.chess.model.boardComponent.Move
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
 @Singleton
-class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController with Observer {
+  val board = Board()
+  val controller = Controller(board)
 
-  def chessAsText = Board().startPos()
+  controller.add(this)
 
-  def displayChessboard = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index(chessAsText))
+  def home = Action {
+    Ok(views.html.index("hello"))
+  }
+
+  def newGame = Action {
+    controller.doAndPublish(controller.newGame)
+    Ok(views.html.index(controller.toString))
+  }
+
+  def move(from: String, to: String) = Action {
+    controller.doAndPublish(controller.makeMove, Move(from, to))
+    Ok(views.html.index(controller.toString))
+  }
+
+  def update(e: Event): Unit = e match {
+    case Event.Quit =>
+    case Event.Move =>
   }
 }
