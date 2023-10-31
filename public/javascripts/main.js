95538c51fef138abcfbef885a4ac8a9233064b40
ZdravkoMat <zd.matic96@googlemail.com>
2023-10-31 09:19:22 +0100

    added some css and bootstrap

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,8 +4,13 @@ var button = document.getElementById("submitButton");
 button.addEventListener("click", function() {
     var fromValue = document.getElementById("from").value;
     var toValue = document.getElementById("to").value;
+    var url = "";
 
-    var url = "/play/move/" + fromValue + "/" + toValue;
-
+    if(fromValue == "" || toValue == ""){
+        url = "/play/move/empty/empty"
+    } else {
+        url = "/play/move/" + fromValue + "/" + toValue;
+    }
+    
     window.location.href = url;
 });
