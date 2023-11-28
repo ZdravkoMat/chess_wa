ad75ec13b8ab64c0774d7401f62d99cb427bf986
ZdravkoMat <zd.matic96@googlemail.com>
2023-11-28 08:33:10 +0100

    published branch for websockets

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -73,3 +73,24 @@ function moveOptions(from) {
 		}
 	});
 }
+
+function connectWebSocket() {
+	var websocket = new WebSocket("ws://localhost:9000/websocket");
+	websocket.setTimeout
+
+	websocket.onopen = function(event) {
+		console.log("Connection established!");
+	};
+
+	websocket.onclose = function(event) {
+		console.log("Connection closed!");
+	};
+
+	websocket.onerror = function(event) {
+		console.log("Error: " + event.data);
+	};
+
+	websocket.onmessage = function(event) {
+		console.log("Message received: " + event.data);
+	};
+}
